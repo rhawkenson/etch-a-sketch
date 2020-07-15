b059639d547d788cebccdc6adb55d427216e3a70
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-07-15 11:15:20 -0500

    Create function to allow a random grid between 1 and 100 grid squares to be created instead of reseting to 16 squares. Change onclick function from firing in html to firing in js. Clear the innerHTML of the gridContainer to create only NEW squares, not add to existing squares

--- script.js
+++ script.js
@@ -1,41 +1,61 @@
 const gridContainer = document.getElementById('grid-container');
-const rebuild = document.getElementById('rebuild');
+const build = document.getElementById('build');
+const reset = document.getElementById('reset');
 const black = document.getElementById('black');
 const eraser = document.getElementById('eraser');
 const rainbow = document.getElementById('rainbow');
 const pink = document.getElementById('pink');
 const green = document.getElementById('green');
 const grayscale = document.getElementById('grayscale');
-
 const user = document.getElementById('user');
 
+gridContainer.style.gridTemplateColumns = `repeat(16, 1fr)`;
+gridContainer.style.gridTemplateRows = `repeat(16, 1fr)`;
+
+build.onclick = () => {
+    gridContainer.innerHTML = '';
+    getGrid();
+}
+    
 function getGrid(){
     let input = document.getElementById('input').value;
     console.log(input);
-    
-    
+
     for(let i=0; i<input*input; i++){
         const squares = document.createElement('div');
         squares.classList.add('squares');
         gridContainer.appendChild(squares);
-        squares.style.border = '1px solid grey';
+        squares.style.border = '.5px solid grey';
         squares.style.width = '100%';
-
-        //gridContainer.style.gridTemplateColumns = `repeat(${input}, 1fr)`;
-        //gridContainer.style.gridTemplateRows = `repeat(${input}, 1fr)`;
-    } hover();
+        gridContainer.style.gridTemplateColumns = `repeat(${input}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${input}, 1fr)`;
+    } 
 }
 
-function hover(){
-    gridContainer.onmouseover = () => {
-        let gridBoxes = document.getElementsByClassName('squares');
-        gridBoxes.style.backgroundColor = 'black';
-    }
+
+reset.onclick = () => {
+    gridContainer.innerHTML = '';
+    resetSketch();
 }
 
 function resetSketch(){
-    window.location.reload();
-}
+   let reset = Math.floor(Math.random()*100);
+    console.log(reset);
 
-gridContainer.style.gridRows = `repeat(${input}, 1fr)`;
-gridContainer.style.gridColumn  = `repeat(${input}, 1fr)`;
+    for(let i=0; i<reset*reset; i++){
+        const resetSquares = document.createElement('div');
+        resetSquares.classList.add('reset-squares');
+        gridContainer.appendChild(resetSquares);
+        resetSquares.style.border = '.5px solid grey';
+        resetSquares.style.width = '100%';
+        gridContainer.style.gridTemplateColumns = `repeat(${reset}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${reset}, 1fr)`;
+    } hover();
+} 
+
+
+function hover(){
+    gridContainer.onmouseover = () => {
+        squares.style.backgroundColor = 'black';
+    }
+}
